66cdae1966cea771b57d9ef6efb99dfa93534915
ukh <able.kiho.um@gmail.com>
2019-03-07 23:54:19 +0900

    oauth2 ...

--- src/main/java/com/web/SpringBootCommunityWebApplication.java
+++ src/main/java/com/web/SpringBootCommunityWebApplication.java
@@ -1,14 +1,48 @@
 package com.web;
 
+import com.web.domain.Board;
+import com.web.domain.User;
+import com.web.repository.BoardRepository;
+import com.web.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class SpringBootCommunityWebApplication {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         SpringApplication.run(SpringBootCommunityWebApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner runner(UserRepository userRepository,
+                                    BoardRepository boardRepository) throws Exception {
+        /*스프링은 빈으로 생성된 메서드에 파라미터로 DI 시키는 메커니즘이 존재.
+        * 생성자를 통해 의존성을 주입시키는 방법과 유사.
+        * 이를 이용하여 CommandLineRunner를 빈으로 등록한 후 UserRepository와 BoardRepository를 주입받는다.*/
+        return (args) -> {
+            User user = userRepository.save(User.builder()
+            .name("havi")
+            .password("test")
+            .email("havi@gmail.com")
+            .createdDate(LocalDateTime.now())
+            .build());
+
+            IntStream.rangeClosed(1,200).forEach(index ->
+                    boardRepository.save(Board.builder()
+                    .title("게시글"+index)
+                    .subTitle("순서"+index)
+                    .content("콘텐츠")
+                    .createdDate(LocalDateTime.now())
+                    .updatedDate(LocalDateTime.now())
+                    .user(user).build()));
+        };
     }
 }
 
--- src/main/java/com/web/service/BoardService.java
+++ src/main/java/com/web/service/BoardService.java
@@ -18,12 +18,15 @@ public class BoardService {
     }
 
     public Page<Board> findBoardList(Pageable pageable) {
-        pageable = PageRequest.of(pageable.getPageNumber() <= 0? 0: pageable.getPageNumber() -1, pageable.getPageSize());
+        /*pageable = PageRequest.of(pageable.getPageNumber() <= 0? 0: pageable.getPageNumber() -1, pageable.getPageSize());
+        return boardRepository.findAll(pageable);*/
+        pageable = new PageRequest(pageable.getPageNumber() <= 0 ? 0 : pageable.getPageNumber() - 1, pageable.getPageSize());
         return boardRepository.findAll(pageable);
     }
 
     public Board findBoardByIdx(Long idx) {
-        return boardRepository.findById(idx).orElse(new Board());
+        //return boardRepository.findById(idx).orElse(new Board());
+        return boardRepository.findOne(idx);
     }
 
 }
